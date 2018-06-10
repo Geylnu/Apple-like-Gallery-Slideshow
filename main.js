c4b8e64af64324693378825a9c7cb13cf097771d
geylnu <yanglei1997630@gmail.com>
2018-06-10 15:42:26 +0800

    第一个版本

---
+++ main.js
@@ -0,0 +1,28 @@
+let time = 6000
+let n = 1
+let id = setAutoplay()
+bindEvent()
+
+function setAutoplay(){
+    return setInterval(()=>{
+        $('.slideShow-wrapper>footer>ul>li').eq(1+n%4).trigger('click')
+        n++
+    },time)
+}
+
+function bindEvent(){
+    $('.slideShow-wrapper>footer>ul>li').on('click',(e)=>{
+        let index = $(e.currentTarget).index()
+        if(index === 0) {return undefined}
+        $('.slideShow-wrapper>footer>ul>li').removeClass('active')
+        $(e.currentTarget).addClass('active')
+        $('.slides').css(
+                {
+                transform : `translateX(${-(index-1)*920}px)`
+                }
+            )
+    })
+    
+    $('.slideShow-wrapper').on('mouseenter',()=>{window.clearInterval(id)})
+        .on('mouseleave',()=>{id=setAutoplay()})
+}
